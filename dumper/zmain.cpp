7996b6c085a185131bdd8d3ec3cb59489e4e3112
duccom0123 <conngunay3@gmail.com>
2025-03-29 23:47:34 +0700

    video 1

--- dumper/zmain.cpp
+++ dumper/zmain.cpp
@@ -18,6 +18,34 @@ using namespace std;
 using namespace std::chrono;
 FILE* f;
 
+__declspec(naked) void HookedStamina()
+{
+	static DWORD jmpBack	= 0x0074B2EC;
+	static DWORD callFunc	= 0x0072EDC0;
+	__asm
+	{
+		push 0xFFEF5F5F
+		lea ecx, [edi + 0x198]
+		call callFunc
+		push 0xFF4497E8
+		lea ecx, [edi + (0x198 + 0x680)]
+		call callFunc
+		push 0xFF1BFA48
+		lea ecx, [edi + (0x198 + 0x680*2)]
+		call callFunc
+
+		jmp jmpBack
+	}
+}
+
+__declspec(naked) void HookedTextAttrs()
+{
+	static DWORD jmpBack	= 0x0074BE56;
+	__asm
+	{
+		jmp jmpBack
+	}
+}
 
 extern "C" _declspec(dllexport) void InitDumper() // OK
 {
@@ -26,7 +54,8 @@ extern "C" _declspec(dllexport) void InitDumper() // OK
 	SetConsoleOutputCP(1252);
 	freopen_s(&f, "CONOUT$", "w", stdout);
 #endif
-	
+	SetOp((LPVOID)0x0074B2CC, (LPVOID)HookedStamina, ASM::JMP);
+	SetOp((LPVOID)0x0074BAD8, (LPVOID)HookedTextAttrs, ASM::JMP);
 
 }
 
